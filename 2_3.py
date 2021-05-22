77ce785ec12c8d0860060363d86e366ae6ce69ef
github oyashota <goyasut@gmail.com>
2021-05-22 14:20:57 +0900

    Nより小さい偶数の数え上げ

--- 2_3.py
+++ 2_3.py
@@ -1,1 +1,26 @@
-print('test')
+#整数Nを受け取り、N以下の偶数を出力する
+
+#func1(N)
+#前から順番にコードは理解していくのでerrorとなる。
+
+class plac:
+    x = 'test'
+
+    def f(self):
+        print(self.x)
+
+    def func1(self,n):
+        for i in reversed(range(n)):
+            if i%2==0 :
+                print(i)
+        return True
+
+    
+
+
+
+
+N = int(input('input integer number : '))
+cl = plac()
+cl.func1(N)
+cl.f()
